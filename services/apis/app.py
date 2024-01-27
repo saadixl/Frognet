c16979e5d121d4a12bb7e3747958c52132b0e7e4
Masudul Haque <saadixl@outlook.com>
2024-01-27 13:08:30 +0800

    Remove webhook code

--- services/apis/app.py
+++ services/apis/app.py
@@ -9,11 +9,6 @@ app = Flask(__name__)
 def root():
     return 'api service working'
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    return data
-
 # Endpoint for reseting task priorities
 @app.route('/reset-tasks', methods=['POST'])
 def reset_tasks():
